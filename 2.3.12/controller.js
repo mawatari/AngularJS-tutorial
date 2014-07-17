34d868466c6dbaf630f8c5b06240b7c9e3043b58
Naoto Mawatari <github.mawatari@gmail.com>
2014-07-17 14:35:01 +0900

    add 2.3.12 2.3.11は、パフォーマンスが良くないので改善

--- 2.3.12/controller.js
+++ 2.3.12/controller.js
@@ -6,24 +6,17 @@ function CartController($scope) {
         {title: "ポルカドット", quantity: 17, price: 12.95},
         {title: "ペブル", quantity: 5, price: 6.95}
     ];
-    
-    $scope.totalCart = function () {
+
+    var calculateTotals = function () {
         var total = 0;
         for (var i = 0, len = $scope.items.length; i < len; i++) {
             total = total + $scope.items[i].price * $scope.items[i].quantity;
         }
+        $scope.bill.total = total;
+        $scope.bill.discount = total > 100 ? 10 : 0;
+        $scope.bill.subTotal = total - $scope.bill.discount;
         console.log('debug');
-
-        return total;
     };
 
-    $scope.subTotal = function () {
-        return $scope.totalCart() - $scope.bill.discount;
-    };
-
-    function calculateDiscount(newValue, oldValue, scope) {
-        $scope.bill.discount = newValue > 100 ? 10 : 0;
-    }
-
-    $scope.$watch($scope.totalCart, calculateDiscount);
+    $scope.$watch("items", calculateTotals, true);
 }
